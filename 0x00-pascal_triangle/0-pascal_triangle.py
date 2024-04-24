1ef7bd6c57baa2c4e334a656389b9117e030b80d
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2024-04-24 21:54:49 +0200

    done

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,14 +10,13 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    pascal_list = []
-    for i in range(n):
-        pascal_list.append([1 for j in range(i + 1)])
-        if i == 0 or i == 1:
-            continue
-        for j in range(n - 1):
-            if j == 0:
-                continue
-            if j < i:
-                pascal_list[i][j] = pascal_list[i-1][j-1] + pascal_list[i-1][j]
-    return pascal_list
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
